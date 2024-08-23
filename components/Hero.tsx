be54894cc7eb2208236b73a5e929f524da3acf01
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-23 13:09:35 +0100

    Added profile pic to button in top left

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className=' w-screen h-screen bg-gradient-to-t bg-slate-300' style={{marginTop: -56}}>
+    <div className=' w-screen h-screen bg-gradient-to-t bg-slate-300' style={{marginTop: -80}}>
 
     </div>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import profilePic from '../public/LewBeavenBar.jpg'
 
 const Navbar = () => {
   return (
-    <nav className='w-screen h-18 bg-indigo-700 flex justify-center items-center'>
-        <a className='size-10 flex-shrink-0 bg-slate-500 ml-10 rounded-full z-10 border-4 border-indigo-300 hover:shadow-md hover:shadow-black' ></a>
-        <ul className='flex justify-start gap-10 items-center w-screen h-14 pl-44'>
-            <a href="#About" className='hover:text-gray-200 hover:text-xl'>About</a>
-            <a href="#Projects" className='hover:text-gray-200 hover:text-xl'>Projects</a>
-            <a href="#Skills" className='hover:text-gray-200 hover:text-xl'>Skills</a>
-            <a href="#Contact" className='hover:text-gray-200 hover:text-xl'>Contact</a>
+    <nav className='w-screen h-20 bg-indigo-700 flex justify-between items-center'>
+      <div className='flex items-center'>
+        <a className='bg-slate-500 rounded-full border-4 border-indigo-300 hover:shadow-md hover:shadow-black flex items-center justify-center ml-10'><Image src={profilePic} alt='Author Picture' className='h-10 w-10 rounded-full'/>
+        </a>
+        <ul className='flex justify-start gap-10 items-center pl-10'>
+          <a href="#About" className='hover:text-gray-200 hover:text-xl'>About</a>
+          <a href="#Projects" className='hover:text-gray-200 hover:text-xl'>Projects</a>
+          <a href="#Skills" className='hover:text-gray-200 hover:text-xl'>Skills</a>
+          <a href="#Contact" className='hover:text-gray-200 hover:text-xl'>Contact</a>
         </ul>
-        <button className='h-10 w-44 rounded-3xl bg-indigo-300 mr-10 hover:shadow-md hover:shadow-black'>Download CV</button>
+      </div>
+      <button className='h-10 w-44 rounded-3xl bg-indigo-300 mr-10 hover:shadow-md hover:shadow-black'>
+        Download CV
+      </button>
     </nav>
-  )
+  );
 }
+
 
 export default Navbar
