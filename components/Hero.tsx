635c4870253e8a02c79924ab937787d62c93863c
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-23 16:26:51 +0100

    Added h1 and p elements to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,15 @@ import ParticlesComponent from './Particles';
 
 const Hero = () => {
   return (
-    <div className=' w-screen h-screen bg-gradient-to-t bg-slate-300' style={{marginTop: -80}}>
-      <ParticlesComponent />
+    <div>
+      <div className='-z-10 absolute w-screen h-screen flex items-center justify-start pl-40' style={{marginTop: -80}}>
+        <ParticlesComponent />
+        <div className='pt-20 flex flex-col  h-40 w-120 absolute '>
+          <h1 className='text-white font-bold text-9xl'>Lewis Beaven</h1>
+          <p className='pl-2 text-xl font-semibold'>Modern websites made easy</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
