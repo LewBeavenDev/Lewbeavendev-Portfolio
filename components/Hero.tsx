ba5188017316c42137f9eec3702268ba9f59d952
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-23 15:25:08 +0100

    added particles background to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ParticlesComponent from './Particles';
+
+
 
 const Hero = () => {
   return (
     <div className=' w-screen h-screen bg-gradient-to-t bg-slate-300' style={{marginTop: -80}}>
-
+      <ParticlesComponent />
     </div>
   )
 }
