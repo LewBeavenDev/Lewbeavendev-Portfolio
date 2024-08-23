519ec3ea75fb290f269bbd2ebf80a845114ddb39
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-23 14:45:39 +0100

    animated links and buttons on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,22 +3,37 @@ import Image from 'next/image'
 import profilePic from '../public/LewBeavenBar.jpg'
 import { motion } from "framer-motion"
 
+const bounceAnimProps = () => ({
+  whileHover: { scale: 1.1 },
+  transition: { type: 'spring', stiffness: 300 }
+});
+
 const Navbar = () => {
   return (
     <nav className='w-screen h-20 bg-indigo-700 flex justify-between items-center'>
       <div className='flex items-center'>
-        <a className='bg-slate-500 rounded-full border-4 border-indigo-300 hover:shadow-md hover:shadow-black flex items-center justify-center ml-10'><Image src={profilePic} alt='Author Picture' className='h-10 w-10 rounded-full'/>
-        </a>
+        <div className='bg-slate-500 rounded-full border-4 border-indigo-300 flex items-center justify-center ml-10'>
+          <Image src={profilePic} alt='Author Picture' className='h-10 w-10 rounded-full'/>
+        </div>
         <ul className='flex justify-start gap-10 items-center pl-10'>
-          <a href="#About" className='hover:text-gray-200 hover:text-xl'>About</a>
-          <a href="#Projects" className='hover:text-gray-200 hover:text-xl'>Projects</a>
-          <a href="#Skills" className='hover:text-gray-200 hover:text-xl'>Skills</a>
-          <a href="#Contact" className='hover:text-gray-200 hover:text-xl'>Contact</a>
+          <motion.div {...bounceAnimProps()}>
+            <a href="#About">About</a>
+          </motion.div>
+          <motion.div {...bounceAnimProps()}>
+            <a href="#Projects">Projects</a>
+          </motion.div>
+          <motion.div {...bounceAnimProps()}>
+            <a href="#Skills">Skills</a>
+          </motion.div>
+          <motion.div {...bounceAnimProps()}>
+            <a href="#Contact">Contact</a>
+          </motion.div>
+
         </ul>
       </div>
       <motion.button
         className='h-10 w-44 rounded-3xl bg-indigo-300 mr-10'
-        whileHover={{ scale: 1.1 }}
+        {...bounceAnimProps()}
         >
         Download CV
       </motion.button>
