cf7c55ed44d81dbd87d8c96d61bd495d3814ab11
LewBeavenDev <elu95@hotmail.co.uk>
2024-08-24 13:52:37 +0100

    animated hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ParticlesComponent from './Particles';
 import profilePic from '../public/LewBeavenBar.jpg';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+const nameText = "Lewis Beaven".split(" ");
 
 const Hero = () => {
   return (
@@ -12,19 +15,51 @@ const Hero = () => {
       </div>
 
       {/* Text Content */}
-      <div className="flex flex-col justify-center space-y-5 z-10 text-center md:text-left">
-        <h1 className="text-white font-bold text-5xl md:text-7xl lg:text-9xl">Lewis Beaven.</h1>
-        <h2 className="text-xl md:text-2xl font-semibold text-white">
+      <div className="flex flex-col justify-center pl-32 space-y-5 z-10 text-center md:text-left">
+        {nameText.map((el, i) => (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ 
+              duration: 0.5,
+              delay: i / 2,
+            }}
+            key={i}
+            className="text-white font-bold text-5xl md:text-7xl lg:text-9xl">{el}{" "}
+          </motion.span>
+        ))}
+        <motion.h2 
+          initial={{ opacity: 0, y:50 }}
+          animate={{ opacity: 1, y:0 }}
+          transition={{ duration: 1, delay:1, type: 'spring', stiffness: 300 }}
+          className="text-xl md:text-2xl font-semibold text-white">
           Creative Junior Front End Developer Turning Ideas into Code.
-        </h2>
-        <p className="text-slate-300 font-medium text-lg lg:w-2/3 md:text-xl">
+        </motion.h2>
+        <motion.p 
+          initial={{ opacity: 0, y:50 }}
+          animate={{ opacity: 1, y:0 }}
+          transition={{ duration: 1, delay:1.5, type: 'spring', stiffness: 300 }}
+          className="text-slate-300 font-medium text-lg lg:w-2/3 md:text-xl">
           With a unique background in engineering and hospitality, I blend creativity with technical
           skills to build functional and engaging web applications.
-        </p>
+        </motion.p>
       </div>
 
       {/* Profile Picture */}
-      <div className="z-10 flex justify-center mb-10 md:mb-0">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001
+          }
+      }}
+        className="z-10 flex justify-center mb-10 md:mb-0">
         <Image
           src={profilePic}
           alt="Lewis Beaven Profile Picture"
@@ -33,7 +68,7 @@ const Hero = () => {
           height={288}
           priority
         />
-      </div>
+      </motion.div>
       <div></div>
     </div>
   );
